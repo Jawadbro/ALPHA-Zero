76c7ccddaf8c4dec613b6205227db6243d1009da
Jawadbro <jawadbro@gmail.com>
2024-10-02 12:40:56 +0600

    code

---
+++ config.py
@@ -0,0 +1,4 @@
+FIREBASE_CREDENTIALS = "path/to/your/firebase-credentials.json"
+GEMINI_API_KEY = "your_gemini_api_key"
+PORCUPINE_ACCESS_KEY = "your_porcupine_access_key"
+PORCUPINE_KEYWORD_PATH = "path/to/your/porcupine_keyword_file.ppn"
